3f2128b5d91eeb6ac5223b23f06f3b77655f6d87
shivu1919 <shivamthakur.ss129@gmail.com>
2025-01-29 15:13:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function checkTemp(){
+    const icon = document.getElementById('icon')
+    const city = document.getElementById('inp').value
+    const cityname = document.getElementById('cityName')
+    const temp = document.getElementById('temperature')
+    const realfeal = document.getElementById('realfeel')
+    const windspeed = document.getElementById('windspeed')
+    const humidity = document.getElementById('humidity')
+
+    fetch(`http://api.weatherapi.com/v1/current.json?key=412b492bf8de4497ae193119252801&q=${city}`)
+    .then((response)=> response.json())
+    .then((data)=>{
+        cityname.innerHTML=city;
+        temp.innerHTML=data.current.temp_c
+        icon.src=data.current.condition.icon
+        realfeal.innerHTML="Real Feel: "+ data.current.feelslike_c
+        windspeed.innerHTML = "Wind Speed: " + data.current.wind_kph + " km/h"
+        humidity.innerHTML="Humidity: " +data.current.humidity
+        console.log(data)
+    })
+}
